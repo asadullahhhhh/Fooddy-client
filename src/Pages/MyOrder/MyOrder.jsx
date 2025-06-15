f1c8f2f5943273ea4b5edf4463fccf91bbeb98ea
Asadullah <assadulla0172@gmail.com>
2025-06-15 14:01:07 +0600

    compelete the delete ordered food button functionality

--- src/Pages/MyOrder/MyOrder.jsx
+++ src/Pages/MyOrder/MyOrder.jsx
@@ -1,9 +1,11 @@
 import React, { use, useEffect, useState } from 'react';
 import { AuthContext } from '../../Context/ContextProvider';
 import Loading from '../../Shared/Loading/Loading';
-import { MdDelete, MdModeEdit } from "react-icons/md";
-import { Link } from 'react-router';
+import { MdDelete } from "react-icons/md";
 import moment from 'moment/moment';
+import axios from 'axios';
+import Swal from 'sweetalert2';
+import { Link } from 'react-router';
 
 const MyOrder = () => {
 
@@ -17,18 +19,80 @@ const MyOrder = () => {
             .then(data => setOrders(data))
     }, [user])
 
-    // console.log(user);
+    // Handel the delete order-button here
+    const handelOrderDelete = (id, foodId, quantity) => {
+      const idObj = {
+        _id: id,
+        foodId,
+        quantity,
+      };
+
+      const filteredData = orders.filter(
+        (item) => item._id !== id
+      );
+
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You want to cancle your order?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          axios.delete(`http://localhost:5000/ordered-food`, {
+            data : idObj
+          })
+            .then(res => {
+              if(res.data.deletedCount){
+                Swal.fire({
+                  position: "center",
+                  icon: "success",
+                  title: "Your has been cancled",
+                  showConfirmButton: false,
+                  timer: 1500,
+                });
+                  setOrders(filteredData)
+              }
+            })
+
+          // 
+        }
+      });
+
+      
+
+      
+    }
+
+    // console.log(orders)
 
     if(!orders) return <Loading></Loading>
 
+    if(orders.length === 0) return (
+      <div className="min-h-[calc(100vh-309px)] flex items-center justify-center">
+        <div className="space-y-4">
+          <h2 className="text-4xl font-semibold text-gray-600">
+            No order available
+          </h2>
+          <div className='text-center'>
+            <Link to={'/all-foods'}>
+              <button className="btn btn-neutral">order food</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+
     return (
-      <section className="min-h-[calc(100vh-65px)] bg-gray-100">
+      <section className="min-h-[calc(100vh-65px)] bg-gray-100 px-5">
         <div className="max-w-7xl mx-auto pb-[100px]">
           <h2 className="uppercase text-center py-5 font-semibold text-4xl">
             Your orders
           </h2>
 
-          <div className=' overflow-x-auto'>
+          <div className=" overflow-x-auto">
             <table className="table table-zebra w-full text-sm">
               <thead className="bg-gray-200">
                 <tr>
@@ -43,12 +107,13 @@ const MyOrder = () => {
               </thead>
               <tbody>
                 {orders.map((group) => (
-                  <tr
-                    key={group._id}
-                    className="border-b border-b-gray-300"
-                  >
+                  <tr key={group._id} className="border-b border-b-gray-300">
                     <td className="bg-gray-200 text-black font-semibold inline-block">
-                      <img src={group.image} className='w-[55px] h-[55px] object-cover mx-auto rounded-lg' alt="" />
+                      <img
+                        src={group.image}
+                        className="w-[55px] h-[55px] object-cover mx-auto rounded-lg"
+                        alt=""
+                      />
                     </td>
                     <td className="font-semibold text-gray-800">
                       {group.name}
@@ -63,10 +128,17 @@ const MyOrder = () => {
                       {group.owner}
                     </td>
                     <td className="font-semibold text-gray-600">
-                      {moment(group.date).format('LLLL')}
+                      {moment(group.date).format("LLLL")}
                     </td>
                     <td className="flex gap-2 justify-center py-2">
                       <button
+                        onClick={() =>
+                          handelOrderDelete(
+                            group._id,
+                            group.foodId,
+                            group.quantity
+                          )
+                        }
                         className="btn btn-sm bg-red-600 hover:bg-red-700 text-white"
                       >
                         <MdDelete size={20} />
